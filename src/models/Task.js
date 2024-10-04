7cf592c699b2f3ccc1acabd28d749df4d4cfe411
DevNinja56 <danisamim9@gmail.com>
2024-10-04 16:13:38 +0500

    initial commit

---
+++ src/models/Task.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    assignedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["initial_phase", "development", "qa", "production"],
+      default: "initial_phase",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high", "urgent"],
+      default: "medium",
+    },
+    dueDate: {
+      type: Date,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    completedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
